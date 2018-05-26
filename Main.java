5cc155658f5cc767ae57c8b05a9539cdae1e2c35
rbrady98 <rian.brady1998@gmail.com>
2018-05-26 23:01:41 +0100

    Added morse to english method

--- Main.java
+++ Main.java
@@ -10,23 +10,47 @@ public class Main {
             "...", "-", "..-", "...-", ".--", "-..-", // s-x
             "-.--", "--.." //y y-z
         };
-        String pattern = "[\\w\\s]{1,}";
+        String result = englishToMorse("my name is rian", morseAlphabet);
+        System.out.println(result);
+        result = morseToEnglish("-- -.-- / -. .- -- . / .. ... / .-. .. .- -.", morseAlphabet);
+        System.out.println(result);
+    }
+    
+    public static String englishToMorse(String input, String[] morseAlphabet) {
         String result = "";
-        if(args.length != 1) {
-            result += "Usage: java Main.java morse sentence";
-        } else if(!(args[0].matches(pattern))) {
-            result += "Please enter either a \'.\' or a \'-\'";
-        } else {
-            String input = args[0];
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                if(c != ' ') {
-                    result += morseAlphabet[((int) c) - 97] + " ";
-                } else {
-                    result += "/ ";
-                }
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(c != ' ') {
+                result += morseAlphabet[((int) c) - 97] + " ";
+            } else {
+                result += "/ ";
             }
         }
-        System.out.println(result);
+        return result;
+    }
+
+    public static String morseToEnglish(String input, String[] morseAlphabet) {
+        String[] morseWords = input.split("\\/");
+        String result = "";
+        for (String morseWord : morseWords) {
+            String[] morseLetters = morseWord.trim().split(" ");
+            for (String letter : morseLetters) {
+                int morseIndex =  searchForLetter(letter, morseAlphabet);
+                char englishLetter = (char)(morseIndex + 97);
+                result += englishLetter;
+            }
+            result += " ";
+        }
+        return result;
+    }
+
+    public static int searchForLetter(String morse, String[] morseLetters) {
+        int index = 0;
+        for (int i = 0; i < morseLetters.length; i++) {
+            if(morseLetters[i].equals(morse)) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
